1dd3f0c97976d6360b70cbd4d1cf0c504edb8543
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 12:24:22 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -3,6 +3,8 @@ import pandas as pd
 import resampled_mvo
 from datetime import datetime
 
+file = st.file_uploader("파일_선택(CSV)", type=['xlsx', 'xls', 'csv'])
+
 number1 = st.number_input('Efficient Frontier Points')
 st.write(number1)
 
@@ -12,4 +14,3 @@ st.write(number2)
 number3 = st.number_input('Select Target Return(%)')
 st.write(number3)
 
-file = st.file_uploader("파일_선택(CSV)", type=['xlsx', 'xls', 'csv'])
