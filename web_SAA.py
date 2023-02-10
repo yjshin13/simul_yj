054d088cf9de7f5d48e84a13125de921262ca4ec
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 14:44:42 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -4,18 +4,24 @@ import resampled_mvo
 from datetime import datetime
 
 st.set_page_config(layout="wide")
+col1,col2,col3 = st.columns([1,1,1])
 file = st.file_uploader("Upload investment universe & price data", type=['xlsx', 'xls', 'csv'])
 
 if file is not None:
-
+    
     assets = pd.read_excel(file, sheet_name="Daily_price",
                            names=None, dtype={'Date': datetime}, index_col=0, header=0).dropna()
 
     tickers = st.multiselect('', assets.columns, list(assets.columns))
-
-    port_num = st.number_input('Efficient Frontier Points')
-    nSim = st.number_input('Number of Simulations')
-    Target = st.number_input('Select Target Return(%)')
-    Growth = st.slider('Growth', 0, 100, (0,30), 1)
-    Inflation = st.slider('Inflation', 0, 100, (0,10), 1)
-    Fixed_Income = st.slider('Fixed_Income', 0, 100, (60,100), 1)
+    
+    with col1:
+        port_num = st.number_input('Efficient Frontier Points')
+        Growth = st.slider('Growth', 0, 100, (0, 30), 1)
+        
+    with col2:
+        nSim = st.number_input('Number of Simulations')
+        Inflation = st.slider('Inflation', 0, 100, (0, 10), 1)
+    
+    with col3:
+        Target = st.number_input('Select Target Return(%)')
+        Fixed_Income = st.slider('Fixed_Income', 0, 100, (60,100), 1)
