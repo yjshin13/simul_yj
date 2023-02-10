9d962e1d9730e91deb51e31f0f030b09f13b4ede
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 12:23:34 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import resampled_mvo
+from datetime import datetime
 
 number1 = st.number_input('Efficient Frontier Points')
 st.write(number1)
@@ -9,3 +11,5 @@ st.write(number2)
 
 number3 = st.number_input('Select Target Return(%)')
 st.write(number3)
+
+file = st.file_uploader("파일_선택(CSV)", type=['xlsx', 'xls', 'csv'])
