71b71a789a237a9a77a8fe6b009d23d41f3eaaed
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 11:21:39 +0900

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.write("""" # my first app Hello *world!*""")
+st.write("""" # my first app Hello *world!*""")
+st.write("""" # my first app Hello *world!*""")
+st.write("""" # my first app Hello *world!*""")
