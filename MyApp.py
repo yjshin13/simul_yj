0af2daa6615b086daff140c9ecce7ade11e5b725
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 11:32:15 +0900

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pandas as pd
+from webSAA import resampled_mvo
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+from datetime import datetime
+import os
+# os.chdir('C:/Users/신용준/PycharmProjects/pythonProject/webSAA')
+#
+# index_data = pd.read_excel('resample_input_BY_YJSHIN.xlsx', sheet_name="Daily_price",
+#                            names=None, dtype={'Date': datetime}, index_col=0, header=4).dropna()
+# # index_data = index_data[index_data.index >= '2012-10-16']
+# Resampled_EF = resampled_mvo.simulation(index_data)
 
-st.write("""" # my first app Hello *world!*""")
-st.write("""" # my first app Hello *world!*""")
-
+def main():
+    number1 = st.number_input('Efficient Frontier Points')
+    number2 = st.number_input('Number of Simulations')
+    number3 = st.number_input('Select Target Return(%)')
