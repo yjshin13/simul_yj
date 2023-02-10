c8566ffc79664232433cf427b49353fa69406ec6
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 15:41:03 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -4,7 +4,7 @@ import resampled_mvo
 from datetime import datetime
 
 st.set_page_config(layout="wide")
-st.warning('혜린이 안녕 사랑해')
+st.warning('혜린이 안녕')
 
 file = st.file_uploader("Upload investment universe & price data", type=['xlsx', 'xls', 'csv'])
 
@@ -18,20 +18,24 @@ if file is not None:
     st.subheader('Resampling Parameters:')
     col1, col2, col3 = st.columns([1, 1, 1])
 
-    with col1:
-        Growth = st.slider('Growth', 0, 100, (0, 30), 1)
-        port_num = st.number_input('Efficient Frontier Points', value=200)
+    #my_expander = st.expander("Expand", expanded=True)
+
+    with st.container():
+        
+        with col1:
+            Growth = st.slider('Growth', 0, 100, (0, 30), 1)
+            port_num = st.number_input('Efficient Frontier Points', value=200)
 
 
-    with col2:
-        Inflation = st.slider('Inflation', 0, 100, (0, 10), 1)
-        nSim = st.number_input('Number of Simulations', value=200)
+        with col2:
+            Inflation = st.slider('Inflation', 0, 100, (0, 10), 1)
+            nSim = st.number_input('Number of Simulations', value=200)
 
 
-    with col3:
-        Fixed_Income = st.slider('Fixed_Income', 0, 100, (60, 100), 1)
-        Target = st.number_input('Select Target Return(%)', value=4.00)
+        with col3:
+            Fixed_Income = st.slider('Fixed_Income', 0, 100, (60, 100), 1)
+            Target = st.number_input('Select Target Return(%)', value=4.00)
 
-    if st.button("Summit"):
-        st.text("혜린아 오늘 뭐하구놀까")
+        if st.button("Summit"):
+            st.text("혜린아 오늘 뭐하구놀까")
 
