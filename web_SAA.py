e83975e24b96b5d79b9ffa850d3c1671d244c225
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 12:39:42 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -10,9 +10,9 @@ def data(file):
 
     return assets
 
-file = st.file_uploader("파일_선택(CSV)", type=['xlsx', 'xls', 'csv'])
+# file = st.file_uploader("파일_선택(CSV)", type=['xlsx', 'xls', 'csv'])
 
-tickers = ('select', data(file).columns.item())
+# tickers = ('select', data(file).columns.item())
 number1 = st.number_input('Efficient Frontier Points')
 st.write(number1)
 
