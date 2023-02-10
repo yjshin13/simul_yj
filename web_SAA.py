a66c9febb8e44f9c1aba971fde7e67a982c6f0f8
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 13:11:58 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -3,22 +3,21 @@ import pandas as pd
 import resampled_mvo
 from datetime import datetime
 
-def data(file):
+file = st.file_uploader("Upload investment universe & price data", type=['xlsx', 'xls', 'csv'])
+
+if file is not None:
 
     assets = pd.read_excel(file, sheet_name="Daily_price",
                            names=None, dtype={'Date': datetime}, index_col=0, header=4).dropna()
 
-    return assets
+    tickers = ('select', assets.columns)
 
-file = st.file_uploader("파일_선택(CSV)", type=['xlsx', 'xls', 'csv'])
-tickers = ('select', data(file).columns)
+    number1 = st.number_input('Efficient Frontier Points')
+    st.write(number1)
 
-number1 = st.number_input('Efficient Frontier Points')
-st.write(number1)
+    number2 = st.number_input('Number of Simulations')
+    st.write(number2)
 
-number2 = st.number_input('Number of Simulations')
-st.write(number2)
-
-number3 = st.number_input('Select Target Return(%)')
-st.write(number3)
+    number3 = st.number_input('Select Target Return(%)')
+    st.write(number3)
 
