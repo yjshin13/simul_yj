5597a2a40ee654eb94dbee2af639b0c79c9fb89d
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-10 11:33:18 +0900

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -15,5 +15,10 @@ import os
 
 def main():
     number1 = st.number_input('Efficient Frontier Points')
+    st.write(number1)
+
     number2 = st.number_input('Number of Simulations')
+    st.write(number2)
+
     number3 = st.number_input('Select Target Return(%)')
+    st.write(number3)
