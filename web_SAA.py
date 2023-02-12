036e7170c42d8e7e01302b1c587c120aef09fb79
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-13 00:23:32 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -6,6 +6,14 @@ from datetime import datetime
 st.set_page_config(layout="wide")
 # st.warning('혜린이 안녕')
 
+def download_df(data):
+    
+    st.download_button(
+        label="Download data as CSV",
+        data=data.to_csv(),
+        mime='text/csv',
+        file_name='Efficient Frontier.csv')
+    
 file = st.file_uploader("Upload investment universe & price data", type=['xlsx', 'xls', 'csv'])
 
 if file is not None:
@@ -42,11 +50,7 @@ if file is not None:
 
             EF = resampled_mvo.simulation(assets, nSim, nPort)
             # csv = EF.to_csv(index=False).encode('utf-8')
-    #
-    # if EF is not None:
-    #
-    #     st.download_button(
-    #         label="Download data as CSV",
-    #         data=EF.to_csv(),
-    #         mime='text/csv',
-    #         file_name='Efficient Frontier.csv' )
+
+    if EF is not None:
+        
+        download_df(EF)
