ace4d0339ff60cb3a5ae608b052ff677ca746070
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-14 10:39:18 +0900

    Add files via upload

---
+++ backtest_graph.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def line_chart(x, title):
+    mycolors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:brown', 'tab:grey', 'tab:pink',
+                'tab:olive', 'tab:purple']
+    columns = x.columns
+
+    # Draw Plot
+    plt.style.use('seaborn-darkgrid')
+    fig, ax = plt.subplots(1, 1, figsize=(13, 5), dpi=100)
+    # length = np.arange(after_nav.index[0],after_nav.index[-1] + pd.DateOffset(years=1),
+    #                         dtype='datetime64[Y]')
+    length = np.arange(x.index[0], x.index[-1] + pd.DateOffset(years=1),
+                       dtype='datetime64[Y]')
+    if len(x.columns) >= 2:
+
+        ax.fill_between(x.index, y1=x.iloc[:, 0].squeeze().values, y2=0, label=columns[0], alpha=0.3,
+                        color=mycolors[1], linewidth=2)
+        ax.fill_between(x.index, y1=x.iloc[:, 1].squeeze().values, y2=0, label=columns[1], alpha=0.3,
+                        color=mycolors[0], linewidth=2)
+
+    else:
+        ax.fill_between(x.index, y1=x.squeeze().values, y2=0, label=columns, alpha=0.3, color=mycolors[1],
+                        linewidth=2)
+
+    # ax.set_title('Portfolio NAV', fontsize=18)
+    ax.set_xlabel('time', size=10)
+    ax.set_ylabel('index', size=10)
+    ax.set_title(title, size=18)
+    ax.tick_params(labelsize=8)
+
+    ax.set_xticks(length)
+    ax.set_xticklabels(length)
+    ax.tick_params(labelsize=8)
+    plt.xticks(rotation=45)
+    plt.legend(loc='upper left')
+
+    plt.gca().spines["top"].set_alpha(0)
+    plt.gca().spines["bottom"].set_alpha(.3)
+    plt.gca().spines["right"].set_alpha(0)
+    plt.gca().spines["left"].set_alpha(.3)
+
+    ax.margins(x=0, y=0)
+    plt.ylim(x.min().min() - abs(x.max().max() - x.min().min()) * 0.1,
+             x.max().max() + abs(x.max().max() - x.min().min()) * 0.05)
+    #########################[Graph Insert]#####################################
+
+    return ax.figure
