98775d7efd7cbd0475b2c8bee4999c2184004e89
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-14 14:19:58 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -55,7 +55,7 @@ if file is not None:
 
         if st.session_state.summit:
 
-            EF = resampled_mvo.simulation(input_price, st.session_state.nSim, st.session_state.nPort, input_universe, constraint_range)
+            st.session_state.summit.EF = resampled_mvo.simulation(input_price, st.session_state.nSim, st.session_state.nPort, input_universe, constraint_range)
             A = input_universe.copy()
             A.index = input_universe['symbol']
             Result = pd.concat([A.drop(['symbol'], axis=1).T, EF.applymap('{:.6%}'.format)], axis=0, join='outer')
