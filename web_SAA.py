c0bfa172e7623b36b3bcc4cb8538cddd0fec5df6
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-14 10:15:16 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -72,9 +72,13 @@ if file is not None:
             Target_Weight = EF.loc[(EF['EXP_RET'] - Target / 100).abs().idxmin()]\
                             .drop(["EXP_RET", "STDEV"])
 
-            Rebalancing_Wegiht =  pd.DataFrame(Target_Weight,
+            Target_Weight_T = pd.DataFrame(Target_Weight).T
+
+            Rebalancing_Wegiht =  pd.DataFrame(Target_Weight_T,
                                     index=pd.date_range(start=input_price.index[0],
                                     end=input_price.index[-1], freq='D')).fillna(method='bfill')
+
+            Rebalancing_Wegiht.iloc[:,:] = Target_Weight_T
 
             SAA_strategy = bt.Strategy('s1', [bt.algos.RunMonthly(run_on_first_date=True),
                                               # bt.algos.RunAfterDate('2000-01-01'),
