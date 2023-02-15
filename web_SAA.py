f5ee7c147c046888183a31a97d89a2125725c3a1
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-15 16:41:50 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -184,7 +184,7 @@ if file is not None:
                 EF_point = plt.figure(figsize=(20, 10))
 
                 Point = np.full(len(st.session_state.EF),0.3)
-                Point[Target_index] =1
+                Point[Target_index] =0.7
 
                 plt.scatter(st.session_state.EF['STDEV'], st.session_state.EF['EXP_RET'].T,
                             marker='o',
