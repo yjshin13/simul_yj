396bd94ee06fbf8522ac1c6f4bb57c8cb1b11e3f
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-15 14:23:15 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -115,9 +115,6 @@ if file is not None:
             bt_SAA = bt.Backtest(SAA_strategy, st.session_state.input_price)
             res = bt.run(bt_SAA)
 
-            st.session_state.Result2 = pd.concat([res.prices.iloc[1:], res.backtests['s1'].stats.drawdown.iloc[1:]], axis=1)
-            st.session_state.Result2.columns = ['NAV', 'Drawdown']
-
             st.empty()
 
             st.write("Backtest")
@@ -181,8 +178,3 @@ if file is not None:
                 mime='text/csv',
                 file_name='Efficient Frontier.csv')
 
-        st.download_button(
-                label="Simulation Result",
-                data=st.session_state.Result2.to_csv(index=False),
-                mime='text/csv',
-                file_name='Simulation Result.csv')
