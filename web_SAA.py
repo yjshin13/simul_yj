e0234d1dd36039dd9bf32daf1e426b9e27bc6a1e
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-16 12:28:02 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -239,7 +239,7 @@ if file is not None:
                 ax_4.stackplot(st.session_state.EF['EXP_RET'], st.session_state.EF.drop(['EXP_RET', 'STDEV'], axis=1).T,
                                labels = Target_Weight.index, alpha = 0.5, edgecolors="face", linewidths=2)
 
-                ax_4.legend(loc='lower left', fontsize=15)
+                ax_4.legend(loc='lower left', fontsize=13)
                 plt.xticks(fontsize=15)
                 plt.yticks(fontsize=15)
                 plt.xlabel('Return(%)', fontsize=15, labelpad=20)
