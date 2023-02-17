c632a45593c51b8ed5db2f88026a5ce913e17771
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-02-17 10:33:41 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -240,7 +240,7 @@ if file is not None:
                 st.write("Weight vs Return")
                 fig_4, ax_4 = plt.subplots(figsize=(20,10))
                 ax_4.stackplot(st.session_state.EF['EXP_RET']*100, (st.session_state.EF*100).drop(['EXP_RET', 'STDEV'], axis=1).T,
-                               labels = Target_Weight.index, alpha = 0.4, edgecolors="face", linewidths=2)
+                               labels = Target_Weight.index, alpha = 0.4, edgecolors="face", linewidths=2, legend='reverse')
 
                 ax_4.legend(loc='lower left', fontsize=14)
                 plt.xticks(fontsize=15)
@@ -256,7 +256,7 @@ if file is not None:
                 st.write("Weight vs Volatility")
                 fig_3, ax_3 = plt.subplots(figsize=(20,10))
                 ax_3.stackplot(st.session_state.EF['STDEV']*100, (st.session_state.EF*100).drop(['EXP_RET', 'STDEV'], axis=1).T,
-                               labels = Target_Weight.index, alpha = 0.4, edgecolors="face", linewidths=2)
+                               labels = Target_Weight.index, alpha = 0.4, edgecolors="face", linewidths=2, legend='reverse')
 
                 ax_3.legend(loc='lower left', fontsize=14)
                 plt.xticks(fontsize=15)
