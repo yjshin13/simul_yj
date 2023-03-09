e4683149d126a8e265cc8826c244f222665eb5ea
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-03-09 13:07:32 +0900

    Create web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -40,12 +40,12 @@ if file is not None:
 
         with col20:
 
-            start_date = st.date_input("Start", value = input_price.index[0], label_visibility="collapsed")
+            start_date = st.date_input("Start", value = input_price.index[0])
             start_date = datetime.combine(start_date, datetime.min.time())
 
         with col21:
 
-            end_date = st.date_input("End", value = input_price.index[-1], label_visibility="collapsed")
+            end_date = st.date_input("End", value = input_price.index[-1])
             end_date = datetime.combine(end_date, datetime.min.time())
 
 
