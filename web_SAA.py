d510c46679ae6642470d01aa4f778fedb5f7f56b
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-03-09 09:14:09 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -42,18 +42,17 @@ if file is not None:
         with col20:
 
             if st.checkbox('Daily data', value=True):
-
-                monthly = True
-                daily = False
+                
+                daily = True
+                monthly = False
 
 
         with col21:
 
             if st.checkbox('Monthly data', value=False):
-
-                monthly = False
-                daily = True
-
+                
+                daily = False
+                monthly = True
 
 
 
