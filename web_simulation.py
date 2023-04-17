9de6bab87f3b49a2f14fa4fde5a79baa57f965ff
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-17 09:26:22 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -12,8 +12,10 @@ if file is not None:
 
     price = pd.read_excel(file, sheet_name="sheet1",
                            names=None, dtype={'Date': datetime}, index_col=0, header=0).dropna()
-    #select = st.multiselect('Input Assets', price.columns, price.columns)
+    select = st.multiselect('Input Assets', price.columns, price.columns)
 
     with st.form("Input Assets", clear_on_submit=False):
 
         st.subheader("Input Assets:")
+
+        summit = st.form_submit_button("Summit")
