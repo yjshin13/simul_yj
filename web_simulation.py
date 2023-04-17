cb1cc15bf2b999b3f1360dfbf62f31d7b8c6e1fd
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-17 10:59:45 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -20,7 +20,7 @@ if file is not None:
     if st.button('적용'):
         st.session_state.input_list = input_list
         st.session_state.input_price = input_price
-        
+
     #
     # with st.form("Input Assets", clear_on_submit=False):
     #
@@ -63,18 +63,18 @@ if file is not None:
 
             if i % 4 == 1:
                 with col1:
-                    slider[k] = st.slider(str(k), 0, 100)
+                    st.session_state.slider[k] = st.slider(str(k), 0, 100)
 
             if i % 4 == 2:
                 with col2:
-                    slider[k] = st.slider(str(k), 0, 100)
+                    st.session_state.slider[k] = st.slider(str(k), 0, 100)
 
             if i % 4 == 3:
                 with col3:
-                    slider[k] = st.slider(str(k), 0, 100)
+                    st.session_state.slider[k] = st.slider(str(k), 0, 100)
 
             if i % 4 == 0:
                 with col4:
-                    slider[k] = st.slider(str(k), 0, 100)
+                    st.session_state.slider[k] = st.slider(str(k), 0, 100)
 
 
