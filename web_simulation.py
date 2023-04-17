8a9f16931bbe6c7c89c6c01986587e8a8fc5dbfa
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-17 11:25:14 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -79,8 +79,8 @@ if file is not None:
 
         if st.button('Sumulation') or ('slider' in st.session_state):
             st.session_state.slider = slider.tolist()
-            portfolio_port = backtest.simulation(st.session_state.input_price, st.session_state.slider)
-            st.line_chart(portfolio_port)
+            # portfolio_port = backtest.simulation(st.session_state.input_price, st.session_state.slider)
+            # st.line_chart(portfolio_port)
 
             
 
