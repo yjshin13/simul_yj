4b043116c863509b4b7cb2a241be8518dc92acf0
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-18 14:20:48 +0900

    Update backtest_graph.py

--- backtest_graph.py
+++ backtest_graph.py
@@ -11,7 +11,7 @@ def line_chart(x, title):
 
     # Draw Plot
     plt.style.use('seaborn-whitegrid')
-    fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=100)
+    fig, ax = plt.subplots(1, 1, figsize=(20, 10), dpi=100)
     # length = np.arange(after_nav.index[0],after_nav.index[-1] + pd.DateOffset(years=1),
     #                         dtype='datetime64[Y]')
     length = np.arange(x.index[0], x.index[-1] + pd.DateOffset(years=1),
