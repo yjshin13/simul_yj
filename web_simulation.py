6155d5a1d27702ad8a79832364df4dbd361a1f2e
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-19 08:48:56 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -89,7 +89,7 @@ if file is not None:
         col11, col22 = st.columns([4,2])
 
         with col11:
-            st.dataframe(st.session_state.input_price)
+            st.dataframe(st.session_state.input_price, height=10)
 
 
         with col22:
