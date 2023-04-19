9fbe7e9b769bddb96831ecdf3727a6e69fda236b
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-19 09:01:31 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -86,54 +86,52 @@ if file is not None:
 
         st.write(str("Total Weight:   ")+str(slider.sum())+str("%"))
 
-        col11, col22 = st.columns([3,2])
-
-        with col11:
-            st.write("Input Data")
-            st.dataframe(st.session_state.input_price, height=550)
-
-
-        with col22:
-
-            st.write("Correlation Heatmap")
-
-
-
-            # Increase the size of the heatmap.
-            fig = plt.figure(figsize=(10, 8))
-            # plt.rc('font', family='Malgun Gothic')
-            plt.rcParams['axes.unicode_minus'] = False
-
-
-            st.session_state.corr = st.session_state.input_price.pct_change().dropna().corr().round(2)
-            st.session_state.corr.index = pd.Index(st.session_state.corr.index.map(lambda x: str(x)[:7]))
-            st.session_state.corr.columns = st.session_state.corr.index
-            # st.session_state.corr.columns = pd.MultiIndex.from_tuples([tuple(map(lambda x: str(x)[:7], col)) for col in st.session_state.corr.columns])
-
-            heatmap = sns.heatmap(st.session_state.corr, vmin=-1, vmax=1, annot=True,
-                                  cmap='BrBG')
-            #heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 20}, pad=12)
-
-
-            st.pyplot(fig)
-
 
 
             #########################[Graph Insert]#####################################
 
         if st.button('Sumulation') or ('slider' in st.session_state):
+            col11, col22 = st.columns([9, 1])
 
+            with col11:
+                st.write("Input Assets")
+                st.dataframe(st.session_state.input_price)
+
+            with col22:
+                st.write("Portfolio NAV")
+                st.dataframe(st.session_state.portfolio_port)
+
+
+
+                # st.write("Correlation Heatmap")
+                #
+                # # Increase the size of the heatmap.
+                # fig = plt.figure(figsize=(10, 8))
+                # # plt.rc('font', family='Malgun Gothic')
+                # plt.rcParams['axes.unicode_minus'] = False
+                #
+                # st.session_state.corr = st.session_state.input_price.pct_change().dropna().corr().round(2)
+                # st.session_state.corr.index = pd.Index(st.session_state.corr.index.map(lambda x: str(x)[:7]))
+                # st.session_state.corr.columns = st.session_state.corr.index
+                # # st.session_state.corr.columns = pd.MultiIndex.from_tuples([tuple(map(lambda x: str(x)[:7], col)) for col in st.session_state.corr.columns])
+                #
+                # heatmap = sns.heatmap(st.session_state.corr, vmin=-1, vmax=1, annot=True,
+                #                       cmap='BrBG')
+                # # heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 20}, pad=12)
+                #
+                # st.pyplot(fig)
 
             st.session_state.slider = (slider*0.01).tolist()
             st.session_state.portfolio_port = backtest.simulation(st.session_state.input_price, st.session_state.slider)
-            st.write(st.session_state.portfolio_port)
 
             col33, col44,col55 = st.columns([1,4,4])
-            
+
             with col33:
                 
-                st.dataframe(st.session_state.portfolio_port)
+                st.pyplot(backtest_graph2.line_chart(st.session_state.portfolio_port, ""))
 
-            with col44:
 
-               st.pyplot(backtest_graph2.line_chart(st.session_state.portfolio_port, ""))
+            # 
+            # with col44:
+
+
