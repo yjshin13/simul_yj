2dd2c71f781885a34e2a299e59dab42c6167a131
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-19 09:11:00 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -94,6 +94,7 @@ if file is not None:
 
             st.session_state.slider = (slider*0.01).tolist()
             st.session_state.portfolio_port = backtest.simulation(st.session_state.input_price, st.session_state.slider)
+            st.session_state.drawdown = backtest.drawdown(st.session_state.portfolio_port)
 
 
 
@@ -137,6 +138,11 @@ if file is not None:
 
                 st.write("Portfolio NAV")
                 st.pyplot(backtest_graph2.line_chart(st.session_state.portfolio_port, ""))
+
+            with col44:
+
+                st.write("Portfolio Drawdown")
+                st.pyplot(backtest_graph2.line_chart(st.session_state.drawdown, ""))
 
 
             #
