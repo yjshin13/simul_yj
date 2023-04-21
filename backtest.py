e2e2abe86bbbd04d0e1c94d900e769157e8f248d
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-21 15:23:57 +0900

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,168 +1,104 @@
-import backtest
-import streamlit as st
-from datetime import datetime
-import backtest_graph2
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+from stqdm import stqdm
 
-st.set_page_config(layout="wide")
-file = st.file_uploader("Upload investment universe & price data", type=['xlsx', 'xls', 'csv'])
-st.warning('Upload data.')
+def cleansing(assets_data=pd.DataFrame(), alloc=list(), rebal=2):
 
-if file is not None:
+    alloc = pd.DataFrame(alloc).T
 
-    @st.cache
-    def load_data(file_path):
-        df = pd.read_excel(file_path, sheet_name="sheet1",
-                           names=None, dtype={'Date': datetime}, index_col=0, header=0)
-        return df
+    assets_data = pd.DataFrame(assets_data,
+                           index=pd.date_range(start=assets_data.index[0],
+                                                end=assets_data.index[-1], freq='D')).fillna(method='ffill')
 
+    allocation = pd.DataFrame(index=assets_data.index, columns=assets_data.columns)
+    allocation[:] = alloc
 
-    price = load_data(file)
+    if rebal==2:
+        allocation = allocation[allocation.index.is_month_end == True]
 
-    price_list = list(map(str, price.columns))
-    select = st.multiselect('Input Assets', price_list, price_list)
-    input_list = price.columns[price.columns.isin(select)]
-    input_price = price[input_list]
+    if rebal==3:
+        allocation = allocation[allocation.index.is_quarter_end == True]
 
-    if st.button('Summit') or ('input_list' in st.session_state):
+    if rebal==4:
+        allocation = allocation[allocation.index.is_year_end == True]
 
-        input_price = input_price.dropna()
+    return assets_data, allocation
 
-        col40, col41, col42, col43, col44, col45 = st.columns([1, 1, 1, 1, 1, 3])
+def simulation(assets_data, allocation, date='1900-01-01', commission=0, rebal=2):
 
-        with col40:
+    assets_data = assets_data[assets_data.index>=date]
 
-            start_date = st.date_input("Start", value=input_price.index[0])
-            start_date = datetime.combine(start_date, datetime.min.time())
+    if type(allocation)==list:
+        assets_data ,allocation = cleansing(assets_data, allocation, rebal)
 
-        with col41:
+    portfolio = pd.DataFrame(index=assets_data.index, columns=['NAV']).squeeze()
+    portfolio = portfolio[portfolio.index >= allocation.index[0]]
+    portfolio[0] = 100
 
-            end_date = st.date_input("End", value=input_price.index[-1])
-            end_date = datetime.combine(end_date, datetime.min.time())
+    k = 0
+    j_rebal = 0
+    i_rebal=0
 
-        with col42:
+    last_alloc = allocation.iloc[0]
 
-            # st.write("Data Frequency")
-
-            if st.checkbox('Daily Input', value=True):
-                daily = True
-                monthly = False
-                annualization = 252
-                freq = 1
-
-            if st.checkbox('Monthly Input', value=False):
-                daily = False
-                monthly = True
-                annualization = 12
-                freq = 2
-
-        with col43:
-
-            # st.write("Data Frequency")
-
-            if st.checkbox('Daily Rebalancing', value=True):
-                rebal = 1
-
-            if st.checkbox('Monthly Rebalancing', value=False):
-                rebal = 2
+    for i in stqdm(range(0, len(portfolio)-1)):
 
 
+        if portfolio.index[i] in allocation.index:
 
-        with col44:
 
-            if st.checkbox('Quarterly Quarterly', value=False):
-                rebal = 3
+            # cost = (commission / 100) * x[i - 1] * transaction_weight[i - 1]
 
-            if st.checkbox('Yearly Quarterly', value=False):
-                rebal = 4
+            j = assets_data.index.get_loc(portfolio.index[i + 1])
+            k = allocation.index.get_loc(portfolio.index[i])
+            i_rebal = portfolio.index.get_loc(portfolio.index[i])
+            j_rebal = assets_data.index.get_loc(portfolio.index[i])
 
-        st.session_state.input_list = input_list
 
-        if daily == True:
-            st.session_state.input_price = input_price[
-                (input_price.index >= start_date) & (input_price.index <= end_date)]
+            transaction_weight = abs(allocation.iloc[k] - last_alloc).sum()
+            cost = commission * transaction_weight
 
-        if monthly == True:
-            st.session_state.input_price = input_price[(input_price.index >= start_date)
-                                                       & (input_price.index <= end_date)
-                                                       & (input_price.index.is_month_end == True)].dropna()
+            print(transaction_weight)
 
-        col1, col2, col3 = st.columns([1, 1, 1])
+            portfolio[i + 1] = portfolio[i_rebal]*(1-cost)*\
+                               (assets_data.iloc[j]/assets_data.iloc[j_rebal] * allocation.iloc[k]).sum()
 
-        slider = pd.Series()
-        #
-        # st.write(input_price.columns)
 
-        col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+            last_alloc = assets_data.iloc[j] / assets_data.iloc[j_rebal] * allocation.iloc[k]
 
-        for i, k in enumerate(st.session_state.input_list, start=0):
+        else:
 
-            if i % 4 == 1:
-                with col1:
-                    slider[k] = st.slider(str(k), 0, 100, 0, 1)
+            j = assets_data.index.get_loc(portfolio.index[i + 1])
 
-            if i % 4 == 2:
-                with col2:
-                    slider[k] = st.slider(str(k), 0, 100, 0, 1)
+            portfolio[i + 1] = portfolio[i_rebal]*(1-cost)*\
+                               (assets_data.iloc[j]/assets_data.iloc[j_rebal] * allocation.iloc[k]).sum()
 
-            if i % 4 == 3:
-                with col3:
-                    slider[k] = st.slider(str(k), 0, 100, 0, 1)
 
-            if i % 4 == 0:
-                with col4:
-                    slider[k] = st.slider(str(k), 0, 100, 0, 1)
+            last_alloc = assets_data.iloc[j] / assets_data.iloc[j_rebal] * allocation.iloc[k]
 
-        st.write(str("Total Weight:   ") + str(slider.sum()) + str("%"))
+    #portfolio.index = portfolio.index.date
 
-        #########################[Graph Insert]#####################################
+    return portfolio.astype('float64').round(3)
 
-        if st.button('Simulation'):
-            st.session_state.slider = (slider * 0.01).tolist()
-            st.session_state.portfolio_port = backtest.simulation(st.session_state.input_price, st.session_state.slider, 0,rebal)
-            st.session_state.drawdown = backtest.drawdown(st.session_state.portfolio_port)
+def drawdown(nav: pd.Series):
+    """
+    주어진 NAV 데이터로부터 Drawdown을 계산합니다.
 
-        if 'slider' in st.session_state:
-            col21, col22, col23 = st.columns([0.8, 0.8, 7])
+    Parameters:
+        nav (pd.Series): NAV 데이터. 인덱스는 일자를 나타내며, 값은 해당 일자의 NAV입니다.
 
-            with col21:
-                st.dataframe(st.session_state.portfolio_port)
+    Returns:
+        pd.Series: 주어진 NAV 데이터로부터 계산된 Drawdown을 나타내는 Series입니다.
+            인덱스는 일자를 나타내며, 값은 해당 일자의 Drawdown입니다.
+    """
+    # 누적 최대값 계산
+    cummax = nav.cummax()
 
-            with col22:
-                st.dataframe(st.session_state.drawdown)
+    # 현재 값과 누적 최대값의 차이 계산
+    drawdown = nav - cummax
 
-            with col23:
-                st.dataframe(st.session_state.input_price)
+    # Drawdown 비율 계산
+    drawdown_pct = drawdown / cummax
 
-            col31, col32 = st.columns([1, 1])
+    drawdown_pct.name = 'MDD'
 
-            with col31:
-                st.write("Portfolio NAV")
-                st.pyplot(backtest_graph2.line_chart(st.session_state.portfolio_port, ""))
-
-            with col32:
-                st.write("Portfolio MDD")
-                st.pyplot(backtest_graph2.line_chart(st.session_state.drawdown, ""))
-
-            col31, col32, col33 = st.columns([2, 3, 5])
-
-            with col33:
-                st.write("Correlation Heatmap")
-
-                # Increase the size of the heatmap.
-                fig = plt.figure(figsize=(15, 8))
-                # plt.rc('font', family='Malgun Gothic')
-                plt.rcParams['axes.unicode_minus'] = False
-
-                st.session_state.corr = st.session_state.input_price.pct_change().dropna().corr().round(2)
-                st.session_state.corr.index = pd.Index(st.session_state.corr.index.map(lambda x: str(x)[:7]))
-                st.session_state.corr.columns = st.session_state.corr.index
-                # st.session_state.corr.columns = pd.MultiIndex.from_tuples([tuple(map(lambda x: str(x)[:7], col)) for col in st.session_state.corr.columns])
-
-                heatmap = sns.heatmap(st.session_state.corr, vmin=-1, vmax=1, annot=True, cmap='BrBG')
-
-                # heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 20}, pad=12)
-
-                st.pyplot(fig)
+    return drawdown_pct
