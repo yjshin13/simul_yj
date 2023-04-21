5b02a8f8c8713e7e4c2756fbd56b94aec673ecf7
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-21 11:20:17 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -95,7 +95,7 @@ if file is not None:
 
             #########################[Graph Insert]#####################################
 
-        if st.button('Sumulation') or ('slider' not in st.session_state):
+        if st.button('Sumulation') or (st.button('Sumulation') and ('slider' not in st.session_state)):
 
             st.session_state.slider = (slider*0.01).tolist()
             st.session_state.portfolio_port = backtest.simulation(st.session_state.input_price, st.session_state.slider)
