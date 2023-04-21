45732576e4263aced107465f97d0adf50625edb6
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-21 16:18:46 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -115,9 +115,11 @@ if file is not None:
 
             st.session_state.portfolio_port.index = st.session_state.portfolio_port.index.date
             st.session_state.drawdown = backtest.drawdown(st.session_state.portfolio_port)
+            st.session_state.input_price.index.date = st.session_state.input_price.index.date
+            st.session_state.allocation.index = st.session_state.allocation.index.date
 
         if 'slider' in st.session_state:
-            col21, col22, col23, col24 = st.columns([0.8, 0.8, 6.2, 0.8])
+            col21, col22, col23, col24 = st.columns([0.8, 0.8, 6.0, 1.0])
 
             with col21:
                 st.dataframe(st.session_state.portfolio_port)
@@ -127,7 +129,7 @@ if file is not None:
 
             with col23:
                 st.dataframe(st.session_state.input_price)
-                
+
             with col24:
                 st.dataframe(st.session_state.allocation)
 
