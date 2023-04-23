3fd68c914e2fad129ca9bcb4dd3ea290ee65b5e8
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-24 00:51:15 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -205,7 +205,7 @@ if file is not None:
                 plt.ylim([0, 0.5])
                 plt.xlabel("Daily Return", size=12)
                 plt.ylabel("Probability", size=12)
-                plt.xticks(np.arange(-0.05, 0.05, 0.01), fontsize=12)
+                plt.xticks(np.arange(Daily_RET.min(), Daily_RET.max(), (Daily_RET.max()-Daily_RET.min())/10), fontsize=12)
                 plt.grid(True, alpha=1, linestyle="--")
                 plt.rcParams["figure.figsize"] = [7, 5]
                 #plt.rcParams["figure.dpi"] = 500
