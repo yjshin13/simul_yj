81e741bad12f0d07ba9a8b7f00981a55f74e750d
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-24 00:33:39 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -198,7 +198,7 @@ if file is not None:
                 #
 
                 fig1 = plt.figure(figsize=(15, 8.8))
-                sns.histplot(data=Daily_RET, bins=99,  color="blue", legend=None, stat="probability", alpha=0.5)
+                sns.histplot(data=Daily_RET, bins=101,  color="blue", legend=None, stat="probability", alpha=0.5)
 
 
                 plt.xlim([-0.05, 0.05])
