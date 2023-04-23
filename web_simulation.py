750f787c82045202c4d936813a030f0af56d3bc5
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-23 10:27:03 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -151,7 +151,7 @@ if file is not None:
 
             with col24:
                 st.write('Allocation')
-                st.dataframe(st.session_state.allocation.applymap('{:.6%}'.format))
+                st.dataframe(st.session_state.allocation.applymap('{:.2%}'.format))
 
 
             col50, col51, col52, col53, col54 = st.columns([1, 1, 1, 1, 1])
