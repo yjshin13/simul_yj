6705b10fb3ce8b6634dba8eab8455f6c16c6776a
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-23 11:00:35 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -190,7 +190,7 @@ if file is not None:
 
             with col_a:
                 # st.write("Heat Ratio")
-                fig1, ax1 = plt.figure(figsize=(15, 8))
+                #fig1, ax1 = plt.figure(figsize=(15, 8))
                 # plt.hist(Daily_RET, bins=50, label="Daily Return", density=True, color="salmon", rwidth=1)
                 # plt.legend()
                 # plt.xticks(np.arange(-0.1, 0.11,0.01), ['{:.1%}'.format(x) for x in np.arange(-0.1, 0.11,0.01)])
@@ -207,8 +207,8 @@ if file is not None:
                 ax1.set_xlabel('Daily_RET')
                 ax1.set_ylabel('Density')
 
-                st.pyplot(fig1)
-                
+                st.pyplot(ax1)
+
             with col_b:
                 st.write("Correlation Heatmap")
 
