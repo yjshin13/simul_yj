da25dd6bdfdcfd8026671d4470e4cfa059e449a4
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-25 01:21:53 +0900

    Create web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -130,7 +130,9 @@ if file is not None:
                 st.session_state.drawdown = backtest.drawdown(st.session_state.portfolio_port)
                 st.session_state.input_price.index = st.session_state.input_price.index.date
                 st.session_state.allocation.index = st.session_state.allocation.index.date
-                st.session_state.result = pd.concat([st.session_state.portfolio_port, st.session_state.drawdown],
+                st.session_state.result = pd.concat([st.session_state.portfolio_port,
+                                                     st.session_state.drawdown, st.session_state.input_price,
+                                                     st.session_state.allocation],
                                                     axis=1)
 
             if 'slider' in st.session_state:
@@ -152,7 +154,7 @@ if file is not None:
 
                     st.download_button(
                         label="NAV",
-                        data=st.session_state.portfolio_port.to_csv(index=True),
+                        data=st.session_state.result.to_csv(index=True),
                         mime='text/csv',
                         file_name='Net Asset Value.csv')
 
