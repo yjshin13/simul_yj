54f393c9550074199e6568ee590227c2c9fa9936
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-24 23:04:32 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -114,7 +114,7 @@ if file is not None:
 
             #########################[Graph Insert]#####################################
 
-            if st.button('Simulation'):
+            if st.button('Simulation') or 'portfolio_port' in st.session_state:
 
 
                 st.session_state.slider = (slider * 0.01).tolist()
