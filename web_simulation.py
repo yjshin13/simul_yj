c6ceb951c900c5dfc0c99d45a43709e6c44617e1
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-25 13:10:02 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -99,19 +99,19 @@ if file is not None:
 
                 if i % 4 == 1:
                     with col1:
-                        slider[k] = st.slider(str(k), 0, 100, weight[k], 1)
+                        slider[k] = st.slider(str(k), 0, 100, weight[k].values, 1)
 
                 if i % 4 == 2:
                     with col2:
-                        slider[k] = st.slider(str(k), 0, 100, weight[k], 1)
+                        slider[k] = st.slider(str(k), 0, 100, weight[k].values, 1)
 
                 if i % 4 == 3:
                     with col3:
-                        slider[k] = st.slider(str(k), 0, 100, weight[k], 1)
+                        slider[k] = st.slider(str(k), 0, 100, weight[k].values, 1)
 
                 if i % 4 == 0:
                     with col4:
-                        slider[k] = st.slider(str(k), 0, 100, weight[k], 1)
+                        slider[k] = st.slider(str(k), 0, 100, weight[k].values, 1)
 
             st.write(str("Total Weight:   ") + str(slider.sum()) + str("%"))
 
