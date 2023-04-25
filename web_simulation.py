d6dbee340d64c1a26e55b5d2e0260ebbbf59e701
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-25 13:22:17 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -120,7 +120,7 @@ if file is not None:
             if st.button('Simulation'):
 
 
-                st.session_state.slider = (slider * 0.01).tolist()
+                st.session_state.slider = (slider).tolist()
                 st.session_state.portfolio_port, st.session_state.allocation = backtest.simulation(st.session_state.input_price,
                                                                                                    st.session_state.slider,
                                                                                                    commission,
