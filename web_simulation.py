66e079358838edd54c3d5a0e07eac5c6f691a837
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-27 15:35:07 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -306,7 +306,7 @@ if file is not None:
 
                     st.pyplot(fig2)
 
-                st.write(st.session_state.alloc_amount.iloc[0,:])
+                st.write(st.session_state.alloc_amount.sum(axis=1))
 
 
         # if 'result' in st.session_state:
