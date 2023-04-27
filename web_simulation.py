c3069ff4cedb09dfac0d03f3cebb0b0533be0c35
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-27 13:32:29 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -249,7 +249,7 @@ if file is not None:
 
                     for bars in bar:
                         width = bars.get_width()
-                        posx = width
+                        posx = width + 0.01
                         posy = bars.get_y() + bars.get_height() * 0.5
                         ax_bar.text(posx, posy, '%.1f' % width, rotation=0, ha='left', va='center', fontsize=13)
 
