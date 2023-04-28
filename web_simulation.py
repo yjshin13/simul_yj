560100faaf8455e26a9ae4cfac2525abbeb5a675
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-28 17:28:31 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -74,7 +74,7 @@ if file is not None:
                 monthly = True
                 annualization = 12
                 freq = 2
-            # 
+            #
             # st.session_state.input_list = input_list.append(pd.Index(['Cash']))
 
             if daily == True:
@@ -112,9 +112,9 @@ if file is not None:
                     with col4:
                         slider[k] = st.slider(str(k), float(0), float(100),  float(weight[k]*100), 0.1)
 
-                
 
-                slider['Cash'] = 1-slider.sum()
+
+                slider.loc['Cash'] = 1-slider.sum()
 
 
 
