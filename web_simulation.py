ce412adeb3109e26d0b9ea219b574acba4c85dbf
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-28 16:54:31 +0900

    Create web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -138,7 +138,7 @@ if file is not None:
                 st.session_state.portfolio_port.index = st.session_state.portfolio_port.index.date
                 st.session_state.drawdown = backtest.drawdown(st.session_state.portfolio_port)
                 st.session_state.input_price.index = st.session_state.input_price.index.date
-                st.session_state.allocation.index = st.session_state.allocation.index.date
+                #st.session_state.allocation.index = st.session_state.allocation.index.date
                 st.session_state.result = pd.concat([st.session_state.portfolio_port,
                                                      st.session_state.drawdown,
                                                      st.session_state.input_price,
