e91d9ee691051f330885b9323502f2fc26bfeda2
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-28 23:46:43 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -130,8 +130,8 @@ if file is not None:
 
 
 
-                st.session_state.contribution = ((st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].iloc[1:]/
-                                                 (st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].shift(1).dropna()))+1).multiply(axis=0)-1
+                st.session_state.contribution = (((st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].iloc[1:]/
+                                                 (st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].shift(1).dropna()))+1).multiply(axis=0))
 
                 st.write(st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].iloc[1:])
                 st.write((st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].shift(1).dropna()))
