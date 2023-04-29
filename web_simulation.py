07ab492fdff32131e6cab1268926afb2f6e1a751
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-29 18:36:22 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -131,7 +131,7 @@ if file is not None:
 
 
                 st.session_state.contribution = ((st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].iloc[1:])/
-                                                 (st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].shift(1).dropna())+1).multiply(axis=0)-1
+                                                 (st.session_state.alloc_amount[st.session_state.alloc_amount.index.is_month_end==True].shift(1).dropna()))
 
                 if monthly == True:
                     st.session_state.portfolio_port = st.session_state.portfolio_port[st.session_state.portfolio_port.index.is_month_end==True]
