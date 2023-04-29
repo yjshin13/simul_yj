35b450f71a556ee684256bfef469a84967b62222
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-30 00:36:56 +0900

    Create web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -153,145 +153,145 @@ if file is not None:
                 st.session_state.alloc.index = st.session_state.alloc.index.date
 
 
-
-
-            if 'slider' in st.session_state:
-
-                START_DATE = st.session_state.portfolio_port.index[0].strftime("%Y-%m-%d")
-                END_DATE = st.session_state.portfolio_port.index[-1].strftime("%Y-%m-%d")
-                Anuuual_RET = round(float(((st.session_state.portfolio_port[-1] / 100) ** (annualization / (len(st.session_state.portfolio_port) - 1)) - 1) * 100), 2)
-                Anuuual_Vol = round(float(np.std(st.session_state.portfolio_port.pct_change().dropna())*np.sqrt(annualization)*100),2)
-                Anuuual_Sharpe = round(Anuuual_RET/Anuuual_Vol,2)
-                MDD  =round(float(min(st.session_state.drawdown) * 100), 2)
-                Daily_RET = st.session_state.portfolio_port.pct_change().dropna()
-
-
-                col21, col22, col23, col24 = st.columns([0.8, 0.8, 3.5, 3.5])
-
-                with col21:
-                    st.write('Result')
-                    st.dataframe(st.session_state.portfolio_port.round(2))
-
-                    st.download_button(
-                        label="Result",
-                        data=st.session_state.result.to_csv(index=True),
-                        mime='text/csv',
-                        file_name='Result.csv')
-
-                with col22:
-                    st.write('MDD')
-                    st.dataframe(st.session_state.drawdown.apply(lambda x: '{:.2%}'.format(x)))
-
-                    # st.download_button(
-                    #     label="MDD",
-                    #     data=st.session_state.drawdown.apply(lambda x: '{:.2%}'.format(x)).to_csv(index=True),
-                    #     mime='text/csv',
-                    #     file_name='MAX Drawdown.csv')
-
-                with col23:
-                    st.write('Assets(normalized)')
-                    st.dataframe((100*st.session_state.input_price/st.session_state.input_price.iloc[0,:]).
-                                  astype('float64').round(2))
-                    #
-                    # st.download_button(
-                    #     label="Assets",
-                    #     data=(100*st.session_state.input_price/st.session_state.input_price.iloc[0,:]).
-                    #               astype('float64').round(2).to_csv(index=True),
-                    #     mime='text/csv',
-                    #     file_name='Assets.csv')
-
-                with col24:
-                    st.write('Allocation(floating)')
-                    st.dataframe(st.session_state.alloc.applymap('{:.2%}'.format))
-                    #
-                    # st.download_button(
-                    #     label="Allocation",
-                    #     data=st.session_state.alloc.applymap('{:.2%}'.format).to_csv(index=True),
-                    #     mime='text/csv',
-                    #     file_name='Allocation.csv')
-
-
-                col50, col51, col52, col53, col54 = st.columns([1, 1, 1, 1, 1])
-
-
-                with col50:
-                    st.info("Period: " + str(START_DATE) + " ~ " + str(END_DATE))
-
-                with col51:
-                    st.info("Annual Return: "+str(Anuuual_RET)+"%")
-
-                with col52:
-                    st.info("Annual Volatility: " + str(Anuuual_Vol) +"%")
-
-                with col53:
-
-                    st.info("Annual Sharpe: " + str(Anuuual_Sharpe))
-
-                with col54:
-
-                    st.info("MDD: " + str(MDD) + "%")
-
-
-                col31, col32 = st.columns([1, 1])
-
-                with col31:
-                    st.write("Net Asset Value")
-                    st.pyplot(backtest_graph2.line_chart(st.session_state.portfolio_port, ""))
-
-                with col32:
-                    st.write("MAX Drawdown")
-                    st.pyplot(backtest_graph2.line_chart(st.session_state.drawdown, ""))
-
-                col_a, col_b, = st.columns([1,1])
-
-
-                with col_a:
-
-                    st.write("Contribution(total "+str(round(100*(st.session_state.contribution).sum(),2))+"%)")
-                    st.session_state.contribution.index = pd.Index(st.session_state.contribution.index.map(lambda x: str(x)[:7]))
-
-
-
-                    x = (st.session_state.contribution * 100)
-                    y = st.session_state.contribution.index
-
-                    fig_bar, ax_bar = plt.subplots(figsize=(18, 10.8))
-                    width = 0.75  # the width of the bars
-                    bar = ax_bar.barh(y, x, color="lightblue", height=0.8, )
-
-                    for bars in bar:
-                        width = bars.get_width()
-                        posx = width + 0.01
-                        posy = bars.get_y() + bars.get_height() * 0.5
-                        ax_bar.text(posx, posy, '%.1f' % width, rotation=0, ha='left', va='center', fontsize=13)
-
-                    plt.xticks(fontsize=15)
-                    plt.yticks(fontsize=15)
-                    plt.xlabel('Contribution(%)', fontsize=15, labelpad=20)
-                    #ax_bar.margins(x=0, y=0)
-
-                    st.pyplot(fig_bar)
-
-
-
-
-
-                with col_b:
-                    st.write("Correlation Matrix")
-
-                    # Increase the size of the heatmap.
-                    fig2 = plt.figure(figsize=(15, 8))
-                    # plt.rc('font', family='Malgun Gothic')
-                    plt.rcParams['axes.unicode_minus'] = False
-
-                    st.session_state.corr = st.session_state.input_price.drop(['Cash'], axis=1).pct_change().dropna().corr().round(2)
-                    st.session_state.corr.index = pd.Index(st.session_state.corr.index.map(lambda x: str(x)[:7]))
-                    st.session_state.corr.columns = st.session_state.corr.index
-                    # st.session_state.corr.columns = pd.MultiIndex.from_tuples([tuple(map(lambda x: str(x)[:7], col)) for col in st.session_state.corr.columns])
-
-                    heatmap = sns.heatmap(st.session_state.corr, vmin=-1, vmax=1, annot=True, cmap='BrBG')
-
-                    # heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 20}, pad=12)
-
-                    st.pyplot(fig2)
+    
+    
+                if 'slider' in st.session_state:
+    
+                    START_DATE = st.session_state.portfolio_port.index[0].strftime("%Y-%m-%d")
+                    END_DATE = st.session_state.portfolio_port.index[-1].strftime("%Y-%m-%d")
+                    Anuuual_RET = round(float(((st.session_state.portfolio_port[-1] / 100) ** (annualization / (len(st.session_state.portfolio_port) - 1)) - 1) * 100), 2)
+                    Anuuual_Vol = round(float(np.std(st.session_state.portfolio_port.pct_change().dropna())*np.sqrt(annualization)*100),2)
+                    Anuuual_Sharpe = round(Anuuual_RET/Anuuual_Vol,2)
+                    MDD  =round(float(min(st.session_state.drawdown) * 100), 2)
+                    Daily_RET = st.session_state.portfolio_port.pct_change().dropna()
+    
+    
+                    col21, col22, col23, col24 = st.columns([0.8, 0.8, 3.5, 3.5])
+    
+                    with col21:
+                        st.write('Result')
+                        st.dataframe(st.session_state.portfolio_port.round(2))
+    
+                        st.download_button(
+                            label="Result",
+                            data=st.session_state.result.to_csv(index=True),
+                            mime='text/csv',
+                            file_name='Result.csv')
+    
+                    with col22:
+                        st.write('MDD')
+                        st.dataframe(st.session_state.drawdown.apply(lambda x: '{:.2%}'.format(x)))
+    
+                        # st.download_button(
+                        #     label="MDD",
+                        #     data=st.session_state.drawdown.apply(lambda x: '{:.2%}'.format(x)).to_csv(index=True),
+                        #     mime='text/csv',
+                        #     file_name='MAX Drawdown.csv')
+    
+                    with col23:
+                        st.write('Assets(normalized)')
+                        st.dataframe((100*st.session_state.input_price/st.session_state.input_price.iloc[0,:]).
+                                      astype('float64').round(2))
+                        #
+                        # st.download_button(
+                        #     label="Assets",
+                        #     data=(100*st.session_state.input_price/st.session_state.input_price.iloc[0,:]).
+                        #               astype('float64').round(2).to_csv(index=True),
+                        #     mime='text/csv',
+                        #     file_name='Assets.csv')
+    
+                    with col24:
+                        st.write('Allocation(floating)')
+                        st.dataframe(st.session_state.alloc.applymap('{:.2%}'.format))
+                        #
+                        # st.download_button(
+                        #     label="Allocation",
+                        #     data=st.session_state.alloc.applymap('{:.2%}'.format).to_csv(index=True),
+                        #     mime='text/csv',
+                        #     file_name='Allocation.csv')
+    
+    
+                    col50, col51, col52, col53, col54 = st.columns([1, 1, 1, 1, 1])
+    
+    
+                    with col50:
+                        st.info("Period: " + str(START_DATE) + " ~ " + str(END_DATE))
+    
+                    with col51:
+                        st.info("Annual Return: "+str(Anuuual_RET)+"%")
+    
+                    with col52:
+                        st.info("Annual Volatility: " + str(Anuuual_Vol) +"%")
+    
+                    with col53:
+    
+                        st.info("Annual Sharpe: " + str(Anuuual_Sharpe))
+    
+                    with col54:
+    
+                        st.info("MDD: " + str(MDD) + "%")
+    
+    
+                    col31, col32 = st.columns([1, 1])
+    
+                    with col31:
+                        st.write("Net Asset Value")
+                        st.pyplot(backtest_graph2.line_chart(st.session_state.portfolio_port, ""))
+    
+                    with col32:
+                        st.write("MAX Drawdown")
+                        st.pyplot(backtest_graph2.line_chart(st.session_state.drawdown, ""))
+    
+                    col_a, col_b, = st.columns([1,1])
+    
+    
+                    with col_a:
+    
+                        st.write("Contribution(total "+str(round(100*(st.session_state.contribution).sum(),2))+"%)")
+                        st.session_state.contribution.index = pd.Index(st.session_state.contribution.index.map(lambda x: str(x)[:7]))
+    
+    
+    
+                        x = (st.session_state.contribution * 100)
+                        y = st.session_state.contribution.index
+    
+                        fig_bar, ax_bar = plt.subplots(figsize=(18, 10.8))
+                        width = 0.75  # the width of the bars
+                        bar = ax_bar.barh(y, x, color="lightblue", height=0.8, )
+    
+                        for bars in bar:
+                            width = bars.get_width()
+                            posx = width + 0.01
+                            posy = bars.get_y() + bars.get_height() * 0.5
+                            ax_bar.text(posx, posy, '%.1f' % width, rotation=0, ha='left', va='center', fontsize=13)
+    
+                        plt.xticks(fontsize=15)
+                        plt.yticks(fontsize=15)
+                        plt.xlabel('Contribution(%)', fontsize=15, labelpad=20)
+                        #ax_bar.margins(x=0, y=0)
+    
+                        st.pyplot(fig_bar)
+    
+    
+    
+    
+    
+                    with col_b:
+                        st.write("Correlation Matrix")
+    
+                        # Increase the size of the heatmap.
+                        fig2 = plt.figure(figsize=(15, 8))
+                        # plt.rc('font', family='Malgun Gothic')
+                        plt.rcParams['axes.unicode_minus'] = False
+    
+                        st.session_state.corr = st.session_state.input_price.drop(['Cash'], axis=1).pct_change().dropna().corr().round(2)
+                        st.session_state.corr.index = pd.Index(st.session_state.corr.index.map(lambda x: str(x)[:7]))
+                        st.session_state.corr.columns = st.session_state.corr.index
+                        # st.session_state.corr.columns = pd.MultiIndex.from_tuples([tuple(map(lambda x: str(x)[:7], col)) for col in st.session_state.corr.columns])
+    
+                        heatmap = sns.heatmap(st.session_state.corr, vmin=-1, vmax=1, annot=True, cmap='BrBG')
+    
+                        # heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 20}, pad=12)
+    
+                        st.pyplot(fig2)
 
