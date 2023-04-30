8565304b328eccfd127dfc073a9e7f227f2afaf6
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-04-30 10:59:45 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -249,7 +249,7 @@ if file is not None:
 
                 with col_a:
 
-                    st.write("Contribution(total "+str(round(100*(st.session_state.contribution).sum(),2))+"%)")
+                    st.write("Contribution(total "+str(round(100*(st.session_state.contribution).sum(),1))+"%)")
                     st.session_state.contribution.index = pd.Index(st.session_state.contribution.index.map(lambda x: str(x)[:7]))
 
 
