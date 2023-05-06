b3f1e1090c2136fcce5f311d42ddce2bb7d417cc
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-05-07 04:22:35 +0900

    Update web_simulation.py

--- web_simulation.py
+++ web_simulation.py
@@ -30,8 +30,8 @@ if file is not None:
     select = st.multiselect('Input Assets', price_list, price_list)
     input_list = price.columns[price.columns.isin(select)]
     input_price = price[input_list]
-    
-    result1 = st.expander('Result', expanded=True)
+
+    st.session_state.result1 = st.expander('Result', expanded=True)
 
 
     if (st.button('Summit') or ('input_list' in st.session_state)):
@@ -178,7 +178,7 @@ if file is not None:
             MDD = round(float(min(st.session_state.drawdown) * 100), 2)
             Daily_RET = st.session_state.portfolio_port.pct_change().dropna()
 
-        with result1:
+        with st.session_state.result1:
 
             if 'slider' in st.session_state:
 
