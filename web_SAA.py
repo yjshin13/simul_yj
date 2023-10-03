dddeceee9c4d937b8d8a0392f2fb3d960c5b333c
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2023-10-04 08:45:23 +0900

    Update web_SAA.py

--- web_SAA.py
+++ web_SAA.py
@@ -13,10 +13,7 @@ st.warning('Upload data.')
 if file is not None:
 
     price = pd.read_excel(file, sheet_name="price",
-                           names=None, dtype={'Date': "datetime64"}, index_col=0, header=0).dropna()
-    # price = pd.read_excel(file, sheet_name="price",
-    #                    names=None, dtype={'Date': datetime}, index_col=0, header=0).dropna()
-    # price = pd.read_excel(file, sheet_name="price", dtype={"your_date_column": "datetime64"})
+                       names=None, dtype={'Date': datetime}, index_col=0, header=0).dropna()
 
 
     universe = pd.read_excel(file, sheet_name="universe",
