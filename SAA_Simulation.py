6f0946890f92476898c67873579fb0766ffe4314
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2024-03-17 01:15:40 +0900

    Update SAA_Simulation.py

--- SAA_Simulation.py
+++ SAA_Simulation.py
@@ -261,7 +261,9 @@ if file is not None:
         st.session_state.Target = Target
 
         st.session_state.Target_alloc = st.session_state.EF[abs(st.session_state.EF['EXP_RET'] - Target) ==
-                                                            min(abs(st.session_state.EF['EXP_RET'] - Target))].drop(columns=['EXP_RET', 'STDEV'])
+                                                            min(abs(st.session_state.EF['EXP_RET'] - Target))].drop(columns=['EXP_RET', 'STDEV']).iloc[0]
+
+
         st.dataframe(st.session_state.Target_alloc)
         st.session_state.Target_alloc['Cash'] = 1 - st.session_state.Target_alloc.sum().sum()
 
