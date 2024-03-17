5bc98eb7aad5ab17b424c3ab4f2ad046f14e2a38
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2024-03-17 21:58:20 +0900

    Update SAA_Simulation.py

--- SAA_Simulation.py
+++ SAA_Simulation.py
@@ -270,7 +270,7 @@ if file is not None:
                 #
                 # st.pyplot(fig_4)
 
-                fig_WE = px.area(st.session_state.EF,x=st.session_state.EF['EXP_RET'], y=st.session_state.EF.iloc[:,5:8])
+                fig_WE = px.area(st.session_state.EF,x=st.session_state.EF['EXP_RET'])
 
                 # fig_WE.update_xaxes(title_text='Time', showgrid=True)
                 # fig.update_yaxes(title_text='NAV', showgrid=True)
