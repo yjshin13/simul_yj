a4792fbf3da96cfe5afa99de0c7b8eef82e7ba03
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2024-03-17 20:24:11 +0900

    Update SAA_Simulation.py

--- SAA_Simulation.py
+++ SAA_Simulation.py
@@ -169,68 +169,68 @@ if file is not None:
             st.empty()
 
 
-            col_a, col_b = st.columns([1, 1])
+            # col_a, col_b = st.columns([1, 1])
+            #
+            # with col_a:
 
-            with col_a:
+            st.write("Efficient Frontier")
+            # EF_point = plt.figure(figsize=(20, 10))
+            #
+            #
+            # Point = np.full(len(st.session_state.EF),0)
+            # Point[Target_index] =2
+            #
+            # plt.scatter(st.session_state.EF['STDEV']*100, (st.session_state.EF['EXP_RET']*100).T,
+            #             marker='o',
+            #             s=130,
+            #             c=Point,
+            #            # alpha=0.7,
+            #             cmap='Paired',
+            #             alpha = 1,
+            #             linewidths=2,
+            #             edgecolors='lightblue')
+            # plt.xticks(fontsize=15)
+            # plt.yticks(fontsize=15)
+            #
+            # plt.xlabel('Expected Risk(%)', fontsize=15, labelpad=20)
+            # plt.ylabel('Expected Return(%)', fontsize=15, labelpad=20)
+            #
+            # st.pyplot(EF_point)
 
-                st.write("Efficient Frontier")
-                # EF_point = plt.figure(figsize=(20, 10))
-                #
-                #
-                # Point = np.full(len(st.session_state.EF),0)
-                # Point[Target_index] =2
-                #
-                # plt.scatter(st.session_state.EF['STDEV']*100, (st.session_state.EF['EXP_RET']*100).T,
-                #             marker='o',
-                #             s=130,
-                #             c=Point,
-                #            # alpha=0.7,
-                #             cmap='Paired',
-                #             alpha = 1,
-                #             linewidths=2,
-                #             edgecolors='lightblue')
-                # plt.xticks(fontsize=15)
-                # plt.yticks(fontsize=15)
-                #
-                # plt.xlabel('Expected Risk(%)', fontsize=15, labelpad=20)
-                # plt.ylabel('Expected Return(%)', fontsize=15, labelpad=20)
-                #
-                # st.pyplot(EF_point)
+            fig_EF = px.scatter(y=st.session_state.EF['EXP_RET'] * 100, x =st.session_state.EF['STDEV'] * 100,template='plotly_dark')
+            fig_EF.update_xaxes(title_text='Standard Deviation',showgrid=True)
+            fig_EF.update_yaxes(title_text='Expected Return',showgrid=True)
+            fig_EF.update_layout(showlegend=False)
+            fig_EF.update_traces(marker=dict(color=['red' if i == Target_index else 'blue' for i in range(len(st.session_state.EF))]))
 
-                fig_EF = px.scatter(y=st.session_state.EF['EXP_RET'] * 100, x =st.session_state.EF['STDEV'] * 100,template='plotly_dark')
-                fig_EF.update_xaxes(title_text='Standard Deviation',showgrid=True)
-                fig_EF.update_yaxes(title_text='Expected Return',showgrid=True)
-                fig_EF.update_layout(showlegend=False)
-                fig_EF.update_traces(marker=dict(color=['red' if i == Target_index else 'blue' for i in range(len(st.session_state.EF))]))
-
-                st.plotly_chart(fig_EF)
-
-            with col_b:
-
-                st.write("Weight")
-                x = (Target_Weight*100).values.round(2)
-                y = Target_Weight.index
-
-                fig_bar, ax_bar = plt.subplots(figsize=(20,10.8))
-                width = 0.75  # the width of the bars
-                bar = ax_bar.barh(y, x, color="lightblue", height= 0.8, )
-
-                for bars in bar:
-                    width = bars.get_width()
-                    posx = width + 0.5
-                    posy = bars.get_y() + bars.get_height() * 0.5
-                    ax_bar.text(posx, posy, '%.1f' % width, rotation=0, ha='left', va='center', fontsize=13)
-
-
-                plt.xticks(fontsize=15)
-                plt.yticks(fontsize=15)
-                plt.xlabel('Weight(%)', fontsize=15, labelpad=20)
-                plt.ylabel('Assets', fontsize=15, labelpad=15)
-                ax_bar.margins(x=0.04, y=0.01)
-
-                st.pyplot(fig_bar)
-
-            col_c, col_d = st.columns([1, 1])
+            st.plotly_chart(fig_EF)
+            #
+            # with col_b:
+            #
+            #     st.write("Weight")
+            #     x = (Target_Weight*100).values.round(2)
+            #     y = Target_Weight.index
+            #
+            #     fig_bar, ax_bar = plt.subplots(figsize=(20,10.8))
+            #     width = 0.75  # the width of the bars
+            #     bar = ax_bar.barh(y, x, color="lightblue", height= 0.8, )
+            #
+            #     for bars in bar:
+            #         width = bars.get_width()
+            #         posx = width + 0.5
+            #         posy = bars.get_y() + bars.get_height() * 0.5
+            #         ax_bar.text(posx, posy, '%.1f' % width, rotation=0, ha='left', va='center', fontsize=13)
+            #
+            #
+            #     plt.xticks(fontsize=15)
+            #     plt.yticks(fontsize=15)
+            #     plt.xlabel('Weight(%)', fontsize=15, labelpad=20)
+            #     plt.ylabel('Assets', fontsize=15, labelpad=15)
+            #     ax_bar.margins(x=0.04, y=0.01)
+            #
+            #     st.pyplot(fig_bar)
+            #
+            # col_c, col_d = st.columns([1, 1])
 
             with col_c:
                 st.write("Weight vs Return")
