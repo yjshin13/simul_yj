c05328e0a1b4f6359f4a940e313c6ba4cd609b69
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2024-03-17 22:47:39 +0900

    Update SAA_Simulation.py

--- SAA_Simulation.py
+++ SAA_Simulation.py
@@ -538,9 +538,9 @@ if file is not None:
                     # st.pyplot(fig_bar)
                     #
                     #
-                    st.session_state.attribution = st.session_state.attribution.drop(st.session_state.attribution.index[-1],axis=0)
+                    st.session_state.attribution2 = st.session_state.attribution.drop(st.session_state.attribution.index[-1],axis=0).copy()
                     st.session_state.pie_data2 = st.session_state.pie_data.copy()
-                    st.session_state.pie_data2.iloc[:, -1] = (st.session_state.attribution*100).round(1)
+                    st.session_state.pie_data2.iloc[:, -1] = (st.session_state.attribution2*100).round(1)
 
 
                     fig_pie2 = px.sunburst(st.session_state.pie_data2, path=['asset_category', 'name'], values=st.session_state.pie_data2.columns[-1])
