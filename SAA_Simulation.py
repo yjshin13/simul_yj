d85af38f6cf23927bd5100f99d7dc1799e37dc06
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2024-03-18 09:16:51 +0900

    Update SAA_Simulation.py

--- SAA_Simulation.py
+++ SAA_Simulation.py
@@ -587,7 +587,7 @@ if file is not None:
 
                     st.session_state.corr = st.session_state.input_price.pct_change().dropna().corr().round(2)        
                     fig_corr = px.imshow(st.session_state.corr,text_auto=True, aspect="auto")
-                    fig_corr.update_layout(width=850)
+                    fig_corr.update_layout(width=820)
                     st.plotly_chart(fig_corr)
 
                 col71, col72 = st.columns([1, 1])
